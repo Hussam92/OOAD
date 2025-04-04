ad52f3567eb5701e528db5636b25d143f4ed0593
Hussam92 <hussam.itani@live.de>
2025-04-04 10:30:55 +0200

    Implement more class attributes

--- test/UserTest.php
+++ test/UserTest.php
@@ -3,16 +3,23 @@
 namespace Test;
 
 use PHPUnit\Framework\TestCase;
-use Publicplan\Model\User;
+use PublicPlan\Model\User;
 
 class UserTest extends TestCase
 {
     public function testUser(): void
     {
         $user = new User('Hussam Itani','03.01.1992');
-        $this->assertInstanceOf(User::class,$user);
-        $this->assertEquals('Hussam Itani', $user->getName());
-        $this->assertTrue(true);
-        $this->assertEquals(1, $user->getBirthday()->format('m'));
+        static::assertInstanceOf(User::class,$user);
+        static::assertEquals('Hussam Itani', $user->getName());
+        static::assertTrue(true);
+        static::assertEquals(3, $user->getBirthday()->format('d'));
+        static::assertEquals(1, $user->getBirthday()->format('m'));
+        static::assertEquals(1992, $user->getBirthday()->format('Y'));
+
+        //static::assertNull($user->getWeight());
+        //static::assertNull($user->getShoeSize());
+        //static::assertNull($user->isHasDriverLicense());
+        //static::assertNull($user->getLanguages());
     }
 }
